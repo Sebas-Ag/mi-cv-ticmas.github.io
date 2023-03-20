987753fe26d6b4a44ce7b35dc16731855f749554
sebachu789 <117925902+sebachu789@users.noreply.github.com>
2023-03-20 06:54:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+
+
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'https://randomuser.me/api/portraits/men/76.jpg') {
+      miImage.setAttribute('src','https://randomuser.me/api/portraits/men/73.jpg');
+    } else {
+      miImage.setAttribute('src', 'https://randomuser.me/api/portraits/men/74.jpg');
+      
+    }
+    if (miSrc === 'https://randomuser.me/api/portraits/men/74.jpg') {
+      miImage.setAttribute('src','https://randomuser.me/api/portraits/men/75.jpg');
+} 
+    if (miSrc === 'https://randomuser.me/api/portraits/men/75.jpg') {
+      miImage.setAttribute('src','https://randomuser.me/api/portraits/men/76.jpg');
+    } 
+}
